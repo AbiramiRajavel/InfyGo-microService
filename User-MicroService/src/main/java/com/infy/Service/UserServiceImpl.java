87317160aab74344deb3e1f930b8fb297feb92fe
Abirami <77911777+AbiramiRajavel@users.noreply.github.com>
2022-08-27 13:18:23 +0530

    Add files via upload

---
+++ User-MicroService/src/main/java/com/infy/Service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.infy.Service;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.infy.Dto.UserDetailsDTO;
+
+import com.infy.Entity.UserDetails;
+import com.infy.Repository.UserRepository;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	UserRepository userRepo;
+	@Autowired
+    ModelMapper mapper;
+	
+	@Override
+	public void userRegister(UserDetailsDTO userdto) {
+		UserDetails user=mapper.map(userdto, UserDetails.class);
+		userRepo.save(user);
+	}
+
+	@Override
+	public String userLogin(UserDetailsDTO userdto) {
+		UserDetails user=mapper.map(userdto, UserDetails.class);
+		Optional<UserDetails> op=userRepo.findById(user.getUserId());
+		if(op.isPresent()) {
+			return  "Successfully Logged in";
+		}
+		return "User Not Found";
+	}
+
+	@Override
+	public UserDetailsDTO checkUser(String userId) {
+		    Optional<UserDetails> user=userRepo.findById(userId);
+		    UserDetailsDTO userDto=null;
+		    if(user.isPresent()) {
+		    	UserDetails user1=user.get();
+		    	userDto=mapper.map(user1, UserDetailsDTO.class);
+		    }
+			return userDto;
+		
+	}
+	
+
+}
---
+++ User-MicroService/src/main/java/com/infy/UserMicroServiceApplication.java
@@ -0,0 +1,20 @@
+package com.infy;
+
+import org.modelmapper.ModelMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class UserMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroServiceApplication.class, args);
+	}
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
+
+}
