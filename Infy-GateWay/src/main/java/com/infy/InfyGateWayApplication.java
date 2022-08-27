c01045f64fbc0eda787398d320c366e2c5e95d92
Abirami <77911777+AbiramiRajavel@users.noreply.github.com>
2022-08-27 13:22:17 +0530

    Add files via upload

---
+++ Infy-GateWay/src/main/java/com/infy/InfyGateWayApplication.java
@@ -0,0 +1,23 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import reactor.netty.http.client.HttpClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class InfyGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyGateWayApplication.class, args);
+	}
+
+	@Bean
+	public HttpClient httpClient() {
+	    return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+	}
+}
